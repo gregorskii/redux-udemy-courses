bd3b72ad7efa32f4cd388d1d060a16c119345bda
Greg Parsons <gregorskii@gmail.com>
2017-03-27 20:29:20 -0700

    fixes cors to use array of allowed origins, which fixes CURL requests

--- redux-auth-api/server/index.js
+++ redux-auth-api/server/index.js
@@ -10,13 +10,7 @@ const app = express();
 
 const whitelist = process.env.CORS_WHITELIST.split(',');
 const corsOptions = {
-  origin (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
+  origin: whitelist
 };
 
 // App setup
